ed671da8bb0e2e3839b4d676c364ca38d1aa1934
Matthew Bendall <bendall@gwu.edu>
2018-08-10 09:56:54 -0400

    regulation scripts

---
+++ scripts/download_regulation_wig.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+from builtins import *
+import sys
+
+lines = [l.strip('\n').split('\t') for l in open('regulation/ENCODE_regulation.tsv', 'rU')]
+header = lines[0]
+lines = lines[1:]
+
+celltypes = sorted(set(l[0] for l in lines))
+for ct in celltypes:
+    print('mkdir -p regulation/%s' % ct)
+
+url_base = 'https://www.encodeproject.org/files/%s/@@download/%s.bigWig'
+
+for l in lines:
+    ct = l[0]
+    assay = l[1]
+    id = l[4]
+    if id == '.':
+        print('no file: %s %s' % (ct, assay), file=sys.stderr)
+        continue
+    dest = 'regulation/%s/%s.%s.bigWig' % (ct, assay, id)
+    dest_short = 'regulation/%s/%s.bigWig' % (ct, assay)
+    url = url_base % (id, id)
+    print('[[ ! -e "%s" ]] && wget -O %s %s' % (dest, dest, url))
+    print('rm -f %s && ln -s %s.%s.bigWig %s' % (dest_short, assay, id, dest_short))    
